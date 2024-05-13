13ddbb94b26cbb4681736b6c27bee27d44912b48
Yanlin852 <ywu289@jh.edu>
2024-05-13 00:59:04 -0400

    wyl

--- page_one.py
+++ page_one.py
@@ -1,1 +1,12 @@
+from utils import get_dataset, selected_dataset
+import streamlit as st
 
+dataset_name = st.selectbox(
+        'Choose a disease to display:',
+        ('Heart Attack', 'Breast Cancer', 'Diabetes') 
+    )
+
+data = get_dataset(dataset_name)
+X, Y = selected_dataset(dataset_name)
+
+st.write(data)
---
+++ utils.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import LabelEncoder
+import streamlit as st
+
+LE = LabelEncoder()
+
+def get_dataset(dataset_name):
+    if dataset_name == "Heart Attack":
+        data = pd.read_csv("https://raw.githubusercontent.com/Yanlin852/DS4PH_final/main/heart.csv")
+        return data
+    elif dataset_name == "Breast Cancer":
+        data = pd.read_csv("https://raw.githubusercontent.com/Yanlin852/DS4PH_final/main/BreastCancer.csv")
+        data["diagnosis"] = LE.fit_transform(data["diagnosis"])
+        data.replace([np.inf, -np.inf], np.nan, inplace=True)
+        data["diagnosis"] = pd.to_numeric(data["diagnosis"], errors="coerce")
+        return data
+    elif dataset_name == "Diabetes":
+        data = pd.read_csv("https://raw.githubusercontent.com/KaitlinZhu/capstone/main/diabetes_data_upload.csv")
+        data["class"] = LE.fit_transform(data["class"])
+        data["class"] = pd.to_numeric(data["class"], errors="coerce")
+        return data
+
+def selected_dataset(dataset_name):
+    data = get_dataset(dataset_name)
+    if dataset_name == "Heart Attack":
+        X = data.drop(["output"], axis=1)
+        Y = data.output
+        return X, Y
+    elif dataset_name == "Breast Cancer":
+        X = data.drop(["id", "diagnosis"], axis=1)
+        Y = data.diagnosis
+        return X, Y
+    elif dataset_name == "Diabetes":
+        if "class" in data.columns:
+            X = data.drop(["class"], axis=1)
+            Y = data['class']
+            return X, Y
+        else:
+            raise ValueError("Column 'class' not found in the dataset")
